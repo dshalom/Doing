77112aa570b592a315cb405509f5b9559eb171d1
David Shalom <david.shalom@sky.uk>
2023-11-11 23:43:07 +0000

    searchui

--- app/src/main/java/com/ds/doing/ui/screens/tasks/TasksContent.kt
+++ app/src/main/java/com/ds/doing/ui/screens/tasks/TasksContent.kt
@@ -1,19 +1,27 @@
 package com.ds.doing.ui.screens.tasks
 
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.scrollable
+import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Done
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material.icons.filled.Star
+import androidx.compose.material3.ElevatedFilterChip
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FilterChipDefaults
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.ListItem
@@ -22,8 +30,10 @@ import androidx.compose.material3.SearchBar
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -39,10 +49,11 @@ fun TasksContent() {
             .padding(8.dp)
     ) {
         FloatingActionButton(
-
             shape = CircleShape,
             onClick = {},
-            modifier = Modifier.align(Alignment.BottomEnd).padding(20.dp)
+            modifier = Modifier
+                .align(Alignment.BottomEnd)
+                .padding(20.dp)
         ) {
             Icon(imageVector = Icons.Filled.Add, contentDescription = "icon")
         }
@@ -62,10 +73,64 @@ fun TasksContent() {
                 )
             }
 
+            item {
+                TaskChips(
+                    titles = listOf("All", "To do", "In Progress", "In Review", "Done"),
+                    modifier = Modifier
+                ) {
+
+                }
+            }
+
         }
     }
 
 }
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TaskChips(
+    titles: List<String>,
+    modifier: Modifier = Modifier,
+    onTaskTypeSelected: (Int) -> Unit
+) {
+    val scrollState = rememberScrollState()
+    var selectedIndex by rememberSaveable {
+        mutableIntStateOf(0)
+    }
+
+    Row(
+        modifier = modifier
+            .fillMaxWidth()
+            .horizontalScroll(scrollState)
+    ) {
+        titles.forEachIndexed { index, title ->
+            TaskChip(title, selectedIndex == index) {
+                selectedIndex = index
+                onTaskTypeSelected(index)
+            }
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TaskChip(title: String, selected: Boolean, onClicked: () -> Unit) {
+    ElevatedFilterChip(
+        selected = selected,
+        onClick = {
+            onClicked()
+        },
+        label = {
+            Text(
+                text = title,
+                style = MaterialTheme.typography.titleMedium,
+            )
+        }
+    )
+
+}
+
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
