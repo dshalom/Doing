11fc481ff9bb074f2a782a63072d70361fe48f9e
David Shalom <david.shalom@sky.uk>
2023-11-12 01:13:07 +0000

    searchui

--- app/src/main/java/com/ds/doing/ui/screens/tasks/TasksContent.kt
+++ app/src/main/java/com/ds/doing/ui/screens/tasks/TasksContent.kt
@@ -1,9 +1,10 @@
 package com.ds.doing.ui.screens.tasks
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.gestures.scrollable
 import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -11,17 +12,23 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyListScope
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.BugReport
+import androidx.compose.material.icons.filled.DirectionsWalk
 import androidx.compose.material.icons.filled.Done
+import androidx.compose.material.icons.filled.EditNote
+import androidx.compose.material.icons.filled.MoreHoriz
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material.icons.filled.Star
 import androidx.compose.material3.ElevatedFilterChip
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.FilterChipDefaults
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.ListItem
@@ -37,9 +44,13 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import com.ds.doing.domain.models.Task
+import com.ds.doing.domain.models.TaskStatus
+import com.ds.doing.domain.models.testTasks
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun TasksContent() {
 
@@ -48,15 +59,7 @@ fun TasksContent() {
             .fillMaxSize()
             .padding(8.dp)
     ) {
-        FloatingActionButton(
-            shape = CircleShape,
-            onClick = {},
-            modifier = Modifier
-                .align(Alignment.BottomEnd)
-                .padding(20.dp)
-        ) {
-            Icon(imageVector = Icons.Filled.Add, contentDescription = "icon")
-        }
+
         LazyColumn(
             modifier = Modifier
                 .fillMaxSize()
@@ -81,13 +84,84 @@ fun TasksContent() {
 
                 }
             }
-
+            taskList(testTasks)
+        }
+        FloatingActionButton(
+            shape = CircleShape,
+            onClick = {},
+            modifier = Modifier
+                .align(Alignment.BottomEnd)
+                .padding(20.dp)
+        ) {
+            Icon(imageVector = Icons.Filled.Add, contentDescription = "icon")
         }
     }
 
 }
 
-@OptIn(ExperimentalMaterial3Api::class)
+fun LazyListScope.taskList(taskList: List<Task>) {
+    items(taskList) { task ->
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(12.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+
+            Box(
+                modifier = Modifier
+                    .size(56.dp)
+                    .clip(CircleShape)
+                    .background(
+                        color = when (task.status) {
+                            TaskStatus.Done -> Color.Green
+                            TaskStatus.InProgress -> Color.Yellow
+                            TaskStatus.InReview -> Color.Magenta
+                            TaskStatus.Todo -> Color.Blue
+                        }
+                    ),
+                contentAlignment = Alignment.Center
+
+            ) {
+                Icon(
+                    modifier = Modifier.size(38.dp),
+                    imageVector = when (task.status) {
+                        TaskStatus.Done -> Icons.Default.Done
+                        TaskStatus.InProgress -> Icons.Default.DirectionsWalk
+                        TaskStatus.InReview -> Icons.Default.BugReport
+                        TaskStatus.Todo -> Icons.Default.EditNote
+                    },
+                    contentDescription = "Add Subject")
+            }
+            Spacer(modifier = Modifier.width(20.dp))
+            Column(modifier = Modifier.weight(1f)) {
+                Text(
+                    text = task.title,
+                    style = MaterialTheme.typography.headlineSmall
+                )
+                Text(
+                    text = task.dateDue,
+                    style = MaterialTheme.typography.headlineSmall
+                )
+            }
+
+
+            Box(modifier = Modifier.size(30.dp),
+                contentAlignment = Alignment.CenterEnd) {
+
+                Icon(
+                    modifier = Modifier.size(40.dp),
+                    imageVector = Icons.Default.MoreHoriz,
+                    contentDescription = "More"
+                )
+            }
+        }
+
+    }
+
+
+}
+
 @Composable
 fun TaskChips(
     titles: List<String>,
